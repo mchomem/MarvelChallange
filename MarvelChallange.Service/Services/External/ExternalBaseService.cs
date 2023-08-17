1eef96d7d5ca2660105c79f3f62baa53dd79ac06
Misael C. Homem <misael.homem@gmail.com>
2023-08-17 14:55:35 -0300

    fix(ExternalBaseService.cs) - Explicit use of types.
    fix(MarvelService.cs) - use of variable to represent information.

--- MarvelChallange.Service/Services/External/ExternalBaseService.cs
+++ MarvelChallange.Service/Services/External/ExternalBaseService.cs
@@ -6,12 +6,13 @@
 
         public async Task<string> SendRequest(string url)
         {
-            var httpClient = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            HttpClient httpClient = new();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
 
-            var response = await httpClient.SendAsync(request);
+            HttpResponseMessage response = await httpClient.SendAsync(request);
             response.EnsureSuccessStatusCode();
-            var responseContent = await response.Content.ReadAsStringAsync();
+
+            string responseContent = await response.Content.ReadAsStringAsync();
             return responseContent;
         }
     }
